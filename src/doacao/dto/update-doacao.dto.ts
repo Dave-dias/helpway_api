b27c21a090ec1fde310ce70a6913877cdda4a02d
Dave-dias <david.augusto.s.dias@gmail.com>
2025-06-08 23:56:32 -0300

    Style: Melhorias

--- src/doacao/dto/update-doacao.dto.ts
+++ src/doacao/dto/update-doacao.dto.ts
@@ -1,16 +1,51 @@
 import { Type } from 'class-transformer';
 import { LocalizacaoUpdateDto } from './update-localizacao.dto';
-import { ApiPropertyOptional } from '@nestjs/swagger';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 
 export class UpdateDoacaoDto {
+  @ApiProperty({
+    example: 'Campanha contra a fome',
+    description: 'Título da doação',
+  })
   titulo?: string;
+
+  @ApiProperty({
+    example: 'Ajude famílias em situação de vulnerabilidade',
+    description: 'Subtítulo da doação',
+  })
   subtitulo?: string;
+
+  @ApiProperty({
+    example: 'Estamos arrecadando para distribuir cestas básicas...',
+    description: 'Descrição detalhada da doação',
+  })
   descricao?: string;
+
+  @ApiProperty({
+    example: 'https://exemplo.com/imagem.jpg',
+    description: 'URL da imagem ilustrativa da doação',
+  })
   imagem_url?: string;
+
+  @ApiProperty({ example: 1000.0, description: 'Meta de doações em reais' })
   meta_doacoes?: number;
+
+  @ApiProperty({ example: 250.5, description: 'Valor já arrecadado em reais' })
   valor_levantado?: number;
+
+  @ApiProperty({ example: true, description: 'Indica se é doação em dinheiro' })
   fg_dinheiro?: boolean;
+
+  @ApiProperty({
+    example: false,
+    description: 'Indica se é doação de alimentação',
+  })
   fg_alimentacao?: boolean;
+
+  @ApiProperty({
+    example: false,
+    description: 'Indica se é doação de vestuário',
+  })
   fg_vestuario?: boolean;
 
   @ApiPropertyOptional({ type: LocalizacaoUpdateDto })
--- src/usuario/dto/update-usuario.dto.ts
+++ src/usuario/dto/update-usuario.dto.ts
@@ -1,4 +1,38 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateUsuarioDto } from './create-usuario-dto';
+import { ApiProperty } from '@nestjs/swagger';
 
-export class UpdateUsuarioDto extends PartialType(CreateUsuarioDto) {}
+export class UpdateUsuarioDto {
+  @ApiProperty({
+    example: 'Ana Paula De Souza',
+    description: 'Nome do usuário',
+  })
+  nome?: string;
+
+  @ApiProperty({
+    example: 'exemplo@gmail.com',
+    description: 'Email do usuário',
+  })
+  email?: string;
+
+  @ApiProperty({
+    example: '123456',
+    description: 'Senha do usuário',
+  })
+  senha?: string;
+
+  @ApiProperty({
+    description: 'Imagem do usuario como string',
+  })
+  img_usuario?: string;
+
+  @ApiProperty({
+    example: '2025-06-01T15:00:00Z',
+    description: 'Data de nascimento em formato ISO 8601',
+  })
+  dt_nascimento?: Date;
+
+  @ApiProperty({
+    example: 1,
+    description: 'Tipo de usuário (1 - Doador, 2 - Destinatario)',
+  })
+  tp_usuario?: number;
+}
