7a8c7d9a990ce79689b0b21d3cde613e7d9ba7ac
Dave-dias <david.augusto.s.dias@gmail.com>
2025-11-09 15:46:05 -0300

    fix: Senha atual crashando requisição de update do usuário

--- src/usuario/mapper/usuario.mapper.ts
+++ src/usuario/mapper/usuario.mapper.ts
@@ -2,5 +2,7 @@ import { Usuario } from '@prisma/client';
 import { UsuarioResponseDto } from '../dto/usuario-response.dto';
 
 export function toUsuarioResponseDto(usuario: Usuario): UsuarioResponseDto {
-  return usuario;
+  return {
+    ...usuario,
+  };
 }
